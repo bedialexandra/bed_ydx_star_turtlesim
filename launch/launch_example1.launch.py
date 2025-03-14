0b3c9f62442bcc750ae545cc3658b7cc46d04231
bedialexandra <bedialexandra2003@gmail.com>
2025-03-14 11:44:01 +0100

    rename user and package names

--- launch/launch_example1.launch.py
+++ launch/launch_example1.launch.py
@@ -25,7 +25,7 @@ def generate_launch_description():
             ]
         ),
         # Node(
-        #     package='ros2_cpp_template',
+        #     package='bed_ydx_star_turtlesim',
         #     executable='simple_sub_node',
         #     output='screen',
         # ),
